a286a5482515788966ac8de833dde0c207662aa4
FadySameh20 <fadysamehmishriki@gmail.com>
2022-10-14 02:19:29 +0200

    Frontend and Backend of Register and Login screens

--- lib/shared/widgets/auth_cubit.dart
+++ lib/shared/widgets/auth_cubit.dart
@@ -1,3 +1,5 @@
+import 'package:ar_furniture_app/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'auth_states.dart';
@@ -6,16 +8,37 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class AuthCubit extends Cubit<AuthStates> {
   AuthCubit() : super(AuthInitialState());
 
-  void login (String email, String password) async {
+  void login(String email, String password) async {
     await FirebaseAuth.instance
-        .signInWithEmailAndPassword(
-        email: email,
-        password: password).
-    then((value) {
+        .signInWithEmailAndPassword(email: email, password: password)
+        .then((value) {
       emit(AuthSuccessfullyState());
     }).catchError((error) {
       emit(AuthErrorState());
     });
   }
 
+  void register(String firstName, String lastName, String email,
+      String password, String address, String mobileNumber) async {
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password)
+        .then((value) async {
+          String userId = FirebaseAuth.instance.currentUser!.uid;
+          UserModel registeredUser = UserModel(
+              fName: firstName,
+              lName: lastName,
+              phone: mobileNumber,
+              address: address,
+              uid: userId,
+              email: email);
+          await FirebaseFirestore.instance
+              .collection('user')
+              .doc(userId)
+              .set(registeredUser.toMap())
+              .then((value) => emit(AuthSuccessfullyState()))
+              .catchError((error) => emit(AuthErrorState()));
+    }).catchError((error) {
+      emit(AuthErrorState());
+    });
+  }
 }
--- lib/shared/widgets/register_screen.dart
+++ lib/shared/widgets/register_screen.dart
@@ -1,6 +1,12 @@
+import 'package:ar_furniture_app/shared/widgets/auth_cubit.dart';
+import 'package:ar_furniture_app/shared/widgets/auth_states.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import '../constants/constants.dart';
+import 'input_text_field.dart';
 
 class RegisterScreen extends StatefulWidget {
   @override
@@ -8,121 +14,257 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  var formKey = GlobalKey<FormState>();
+  var firstNameController = TextEditingController();
+  var lastNameController = TextEditingController();
   var emailController = TextEditingController();
   var passController = TextEditingController();
+  var confirmPassController = TextEditingController();
+  var addressController = TextEditingController();
+  var mobileNumberController = TextEditingController();
+  bool isPasswordHidden = false;
+  bool isLoading = false;
+  String initialCountry = 'EG';
+  PhoneNumber number = PhoneNumber(isoCode: 'EG');
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  height: MediaQuery.of(context).size.height / 2,
-                  child: Material(
-                    elevation: 5,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
+    return BlocProvider<AuthCubit>(
+      create: (context) => AuthCubit(),
+      child: BlocConsumer<AuthCubit, AuthStates>(
+        listener: (context, state) {
+          if (state is AuthSuccessfullyState) {
+            print("Registered Successfully");
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text('Regiseterd Successfully !'),
+            ));
+            Navigator.pushNamedAndRemoveUntil(
+                context, '/home', (route) => false);
+          } else if (state is AuthErrorState) {
+            print("Error in registration");
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text('Error in registration !'),
+            ));
+            setState(() {
+              isLoading = false;
+            });
+          }
+        },
+        builder: (context, state) {
+          return Scaffold(
+            body: isLoading
+                ? Center(
+                    child: CircularProgressIndicator(
+                    color: kAppBackgroundColor,
+                    strokeWidth: 5,
+                  ))
+                : Stack(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height / 4.6,
+                            left: MediaQuery.of(context).size.width / 3.5),
+                        child: Text(
                           "Register",
                           style: TextStyle(
                             fontSize: 40,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        TextFormField(
-                          controller: emailController,
-                          decoration: InputDecoration(hintText: "Email"),
-                          validator: (value) {
-                            if (value!.isEmpty) return "You entered nothing";
-                          },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height / 3.45,
                         ),
-                        TextField(
-                          controller: passController,
-                          decoration: InputDecoration(hintText: "Password"),
-                          obscureText: true,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Center(
+                                child: Container(
+                                  margin: EdgeInsets.all(30.0),
+                                  height:
+                                      MediaQuery.of(context).size.height / 2,
+                                  child: Material(
+                                    borderRadius: BorderRadius.circular(20),
+                                    elevation: 5,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Form(
+                                        child: SingleChildScrollView(
+                                          child: Column(
+                                            children: [
+                                              InputTextField(
+                                                textController:
+                                                    firstNameController,
+                                                hint: "First Name",
+                                                prefixIconData: Icons.person,
+                                              ),
+                                              InputTextField(
+                                                textController:
+                                                    lastNameController,
+                                                hint: "Last Name",
+                                                prefixIconData: Icons.person,
+                                              ),
+                                              InputTextField(
+                                                textController: emailController,
+                                                hint: "Email",
+                                                prefixIconData: Icons.email,
+                                              ),
+                                              InputTextField(
+                                                textController: passController,
+                                                hint: "Password",
+                                                prefixIconData: Icons.password,
+                                              ),
+                                              InputTextField(
+                                                textController:
+                                                    confirmPassController,
+                                                hint: "Confirm Password",
+                                                prefixIconData: Icons.password,
+                                              ),
+                                              InputTextField(
+                                                textController:
+                                                    addressController,
+                                                hint: "Address",
+                                                prefixIconData:
+                                                    Icons.add_location,
+                                              ),
+                                              Padding(
+                                                padding: const EdgeInsets.only(
+                                                    top: 13.0,
+                                                    bottom: 12.0,
+                                                    left: 12.0,
+                                                    right: 8.0),
+                                                child:
+                                                    InternationalPhoneNumberInput(
+                                                  spaceBetweenSelectorAndTextField:
+                                                      0,
+                                                  onInputChanged:
+                                                      (PhoneNumber number) {},
+                                                  selectorConfig:
+                                                      const SelectorConfig(
+                                                    selectorType:
+                                                        PhoneInputSelectorType
+                                                            .BOTTOM_SHEET,
+                                                  ),
+                                                  ignoreBlank: false,
+                                                  autoValidateMode:
+                                                      AutovalidateMode
+                                                          .onUserInteraction,
+                                                  selectorTextStyle:
+                                                      const TextStyle(
+                                                          color: Colors.black),
+                                                  initialValue: number,
+                                                  textFieldController:
+                                                      mobileNumberController,
+                                                  formatInput: false,
+                                                  keyboardType:
+                                                      const TextInputType
+                                                              .numberWithOptions(
+                                                          signed: true,
+                                                          decimal: true),
+                                                  inputBorder:
+                                                      const OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.all(
+                                                            Radius.circular(
+                                                                20.0)),
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 2.0,
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isLoading = true;
+                                  });
+                                  BlocProvider.of<AuthCubit>(context).register(
+                                      firstNameController.text,
+                                      lastNameController.text,
+                                      emailController.text,
+                                      passController.text,
+                                      addressController.text,
+                                      mobileNumberController.text);
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      Color.fromRGBO(191, 122, 47, 1),
+                                ),
+                                child: Text(
+                                  "Register",
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                              SizedBox(
+                                height: MediaQuery.of(context).size.height > 700
+                                    ? 20.0
+                                    : 5.0,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    'Already have an account ?',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  SizedBox(width: 5.0),
+                                  InkWell(
+                                    onTap: () {
+                                      Navigator.pushNamed(context, '/login');
+                                    },
+                                    child: Text(
+                                      'Login',
+                                      style: TextStyle(
+                                          color:
+                                              Color.fromRGBO(191, 122, 47, 1),
+                                          fontWeight: FontWeight.bold,
+                                          decoration: TextDecoration.underline,
+                                          fontSize: 18),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                        ElevatedButton(
-                            onPressed: () async {
-                              print(emailController.text);
-                              print(passController.text);
-                              await FirebaseAuth.instance
-                                  .createUserWithEmailAndPassword(
-                                  email: emailController.text,
-                                  password: passController.text)
-                                  .then((value) {
-                                print("test");
-                                print(value);
-                              }).catchError((error) {
-                                print(error);
-                              });
-                              print("out");
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Color.fromRGBO(191, 122, 47, 1),
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: ClipPath(
+                          clipper: AuthClip(),
+                          child: Container(
+                            height: MediaQuery.of(context).size.height / 2.1,
+                            width: double.infinity,
+                            // color: const Colors.blue,
+                            decoration: const BoxDecoration(
+                              gradient: LinearGradient(colors: [
+                                // 234,
+                                Color.fromRGBO(248, 197, 142, 1.0),
+
+                                Color.fromRGBO(239, 169, 93, 1.0),
+
+                                Color.fromRGBO(191, 122, 47, 1),
+                              ]),
                             ),
-                            child: Text(
-                              "Register",
-                              style: TextStyle(fontSize: 18),
-                            )),
-                      ],
-                    ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom:10,
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    'Already have an account ?',
-                    style: TextStyle(
-                        fontSize: 18
-                    ),
-                  ),
-                  SizedBox(width: 5.0),
-                  InkWell(
-                    onTap: () {},
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                          color: Color.fromRGBO(191, 122, 47, 1),
-                          fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.underline,
-                          fontSize: 18),
-                    ),
-                  ),
-
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment.topRight,
-              child: ClipPath(
-                clipper: AuthClip(),
-                child: Container(
-                  height: MediaQuery.of(context).size.height / 2.1,
-                  width: double.infinity,
-                  // color: const Colors.blue,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(colors: [
-                      // 234,
-                      Color.fromRGBO(248, 197, 142, 1.0),
-
-                      Color.fromRGBO(239, 169, 93, 1.0),
-
-                      Color.fromRGBO(191, 122, 47, 1),
-                    ]),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ));
+          );
+        },
+      ),
+    );
   }
 }
 
