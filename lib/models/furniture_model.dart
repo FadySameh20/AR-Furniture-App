d33d0d02aceef2805e2252b1538d790258fb6c5b
MernaMouradd <es-MernaWilliam2023@alexu.edu.eg>
2022-10-17 20:03:16 +0200

    modified furniture model

--- lib/models/furniture_model.dart
+++ lib/models/furniture_model.dart
@@ -3,38 +3,36 @@ import 'dart:ffi';
 import 'package:ar_furniture_app/models/shared_model.dart';
 
 class FurnitureModel {
-  late String furnitureId;
   String? description;
+  late String furnitureId;
   late String name;
   late String model;
   late List<SharedModel> shared;
 
   FurnitureModel({
-    required this.furnitureId,
     this.description,
+    required this.furnitureId,
     required this.name,
     required this.model,
     required this.shared,
   });
 
   FurnitureModel.fromJson(Map<String, dynamic> json) {
-
     description = json["description"];
+    furnitureId = json["furnitureId"];
     name = json["name"];
     model = json["model"];
-    for (int i = 0; i < shared.length; i++){
-      shared[i] =SharedModel.fromJson(json["shared"][i]);
-
+    for (int i = 0; i < shared.length; i++) {
+      shared[i] = SharedModel.fromJson(json["shared"][i]);
     }
-
   }
   Map<String, dynamic> toMap() {
     return {
       "description": description,
+      "furnitureId": furnitureId,
       "name": name,
       "model": model,
-      "shared":shared,
-
+      "shared": shared,
     };
   }
 }
