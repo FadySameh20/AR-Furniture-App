735f2ca3497a3a741428b17d263d6d686f7f9c79
amrmohamed25 <amrmohamedawed80@gmail.com>
2022-10-17 18:12:24 +0200

    Added get all categories functions

--- lib/cubits/home_cubit.dart
+++ lib/cubits/home_cubit.dart
@@ -3,12 +3,25 @@ import 'package:ar_furniture_app/models/offers_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../models/name_model.dart';
+
 class HomeCubit extends Cubit<HomeState>{
   HomeCubit():super(InitialHomeState());
 
   List<Offers> offers=[];
-
-  void getOffers() async {
+  List<CategoryItem> categories=[];
+   getAllData() async{
+    await getCategoryNames();
+    await getOffers();
+  }
+  getCategoryNames()async{
+     FirebaseFirestore.instance.collection("names").get().then((value) {
+       value.docs.forEach((element) {
+         categories.add(CategoryItem.fromJson(element.data()));
+       });
+     }).catchError((error){});
+  }
+   getOffers() async {
     await FirebaseFirestore.instance.collection('offer')
         .get().then((value){
           value.docs.forEach((element) {
