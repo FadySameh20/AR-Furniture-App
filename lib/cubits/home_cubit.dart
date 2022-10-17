4e95a1490642e2e36b3424271409a4b6d4e44c05
mayarahmedd <mayarahmed.2999@gmail.com>
2022-10-17 17:09:03 +0200

    Added get all offers from Firestore

--- lib/cubits/home_cubit.dart
+++ lib/cubits/home_cubit.dart
@@ -1,7 +1,22 @@
 import 'package:ar_furniture_app/cubits/home_states.dart';
+import 'package:ar_furniture_app/models/offers_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeCubit extends Cubit<HomeState>{
   HomeCubit():super(InitialHomeState());
+
+  List<Offers> offers=[];
+
+  void getoffers() async {
+    await FirebaseFirestore.instance.collection('offer')
+        .get().then((value){
+          value.docs.forEach((element) {
+            offers.add(Offers.fromJson(element.data()));
+          });
+    });
+    print(offers.length);
+
+  }
 
 }
