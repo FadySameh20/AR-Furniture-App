e50a05bb23d37e72fa8ceeff5e7b6a627ef5902b
mayar <mayarahmed.2999@gmail.com>
2023-03-08 23:33:17 +0200

    Revert "Added AR to cart"
    This reverts commit d54c9ad954b9ca2af205f01a47861025c72a1996.

--- lib/shared/widgets/cart_screen.dart
+++ lib/shared/widgets/cart_screen.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:ar_furniture_app/cubits/home_cubit.dart';
 import 'package:ar_furniture_app/cubits/home_states.dart';
-import 'package:ar_furniture_app/models/shared_model.dart';
 import 'package:ar_furniture_app/shared/cache/sharedpreferences.dart';
 import 'package:ar_furniture_app/shared/constants/constants.dart';
 import 'package:ar_furniture_app/shared/widgets/checkout_screen.dart';
@@ -14,8 +13,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../models/furniture_model.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
-
-import 'objectgesturesexample.dart';
 
 class CartScreen extends StatefulWidget {
   // List<FurnitureModel> furnitureList;
@@ -41,8 +38,6 @@ class _CartScreenState extends State<CartScreen> {
   List<String> furnitureIds=[];
   List<String> furnitureColors=[];
   List<String> furnitureColorNames=[];
-  List<FurnitureModel> furnModel=[];
-
 
   var quantity = 0;
   double subTotal = 0;
@@ -76,8 +71,6 @@ class _CartScreenState extends State<CartScreen> {
         print("lol");
         print(element.quantityCart);
         if (int.parse(element.quantityCart) > 0) {
-          FurnitureModel furnModelTemp = FurnitureModel(description:"",furnitureId:key,name:"",category:"",shared:[SharedModel(color:element.color,colorName:"",image:element.image,price:"",quantity:"",discount:"",model:element.model)],ratings:{});
-          furnModel.add(furnModelTemp);
           furnitureQuantities.add(element.quantityCart);
           furnitureImages.add(element.image);
           availableQuantity.add(element.quantity);
@@ -95,7 +88,6 @@ class _CartScreenState extends State<CartScreen> {
               .where((element) => element.furnitureId == key)
               .first
               .furnitureId);
-
 
 
 
@@ -473,11 +465,7 @@ class _CartScreenState extends State<CartScreen> {
                 ),
               ),
               actions: [
-                IconButton(onPressed: (){
-                  Navigator.push(context,MaterialPageRoute(builder: (context)=>ObjectGesturesWidget(furnModel)));
-                }, icon: Icon(Icons.camera)),
-               // IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart,color: BlocProvider.of<HomeCubit>(context).isDark?Colors.black:Colors.white,)),
-
+                IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart,color: BlocProvider.of<HomeCubit>(context).isDark?Colors.black:Colors.white,))
               ],
               centerTitle: true,
 
